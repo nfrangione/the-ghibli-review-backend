56048e157c5061313d51aebbbed2db15c2cdea20
Nicholas V Frangione <nfrangione8@gmail.com>
2021-05-10 14:00:41 -0700

    Routes Added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,17 @@
 class UsersController < ApplicationController
+    def show
+        user = User.find_by(id: params[:id])
+        render json: user, except: [:created_at, :updated_at]
+    end
+
+    def create
+        user = User.create(user_params)
+        render json: user, except: [:created_at, :updated_at]
+    end
+
+    private
+
+    def user_params
+        params.require(:user).require(:username, :password)
+    end
 end
